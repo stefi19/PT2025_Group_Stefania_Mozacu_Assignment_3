21bd015ff150c8e9db622cad44c4b9fcd5fae264
stefi19 <stefaniamozacu1@gmail.com>
2025-05-21 22:36:46 +0300

    made the Bill class a record

--- src/main/java/Model/Bill.java
+++ src/main/java/Model/Bill.java
@@ -1,43 +1,6 @@
 package Model;
 import java.time.LocalDateTime;
-public final class Bill {
-    private final int id;
-    private final String clientName;
-    private final String productName;
-    private final int quantity;
-    private final double totalPrice;
-    private final LocalDateTime createdAt;
-    public Bill(int id, String clientName, String productName, int quantity, double totalPrice, LocalDateTime createdAt)
-    {
-        this.id=id;
-        this.clientName=clientName;
-        this.productName=productName;
-        this.quantity=quantity;
-        this.totalPrice=totalPrice;
-        this.createdAt=createdAt;
-    }
-    public int getId()
-    {
-        return id;
-    }
-    public String getClientName()
-    {
-        return clientName;
-    }
-    public String getProductName()
-    {
-        return productName;
-    }
-    public int getQuantity()
-    {
-        return quantity;
-    }
-    public double getTotalPrice()
-    {
-        return totalPrice;
-    }
-    public LocalDateTime getCreatedAt()
-    {
-        return createdAt;
-    }
+
+public record Bill(int id, String clientName, String productName, int quantity, double totalPrice,
+                   LocalDateTime createdAt) {
 }
